7096394c4b2f757c1163cacc8fbe6690dd4cbea8
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-20 01:15:02 +0530

    Welcome to User Registration Problem

---
+++ src/regex/UserRegistrationMain.java
@@ -0,0 +1,8 @@
+package regex;
+
+public class UserRegistrationMain {
+
+    public static void main(String[] args) {
+        System.out.println(" Welcome to User Registration problem ");
+    }
+}
