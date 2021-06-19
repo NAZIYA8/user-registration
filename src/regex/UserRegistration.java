23b25ba949755fb478420a1cfbb2d4b7757641fc
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-20 01:26:42 +0530

    Added: Check if the entered first name is valid or not

--- src/regex/UserRegistration.java
+++ src/regex/UserRegistration.java
@@ -1,2 +1,39 @@
+/**
+ * ****************************************************************************
+ * Purpose: This class contains a number of methods. It helps the main method
+ *          to call these methods any number of times.
+ *
+ * @author Syeda Naziya
+ * @version 1.0
+ * @since 17-06-2021
+ * ****************************************************************************
+ */
 
+package regex;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+    static Scanner scan = new Scanner(System.in);
+
+    /**
+     * This method is used to take the  first name as input and
+     * check if it is valid or not.
+     */
+    public void validateFirstName() {
+        System.out.println("Enter First Name ");
+        String firstName = scan.nextLine();
+        Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}");
+        Matcher matcher = pattern.matcher((firstName));
+        boolean check = matcher.matches();
+        if (check) {
+            System.out.println("First Name is Valid");
+        } else {
+            System.out.println("First name is invalid");
+        }
+    }
+
+}
--- src/regex/UserRegistrationMain.java
+++ src/regex/UserRegistrationMain.java
@@ -1,8 +1,22 @@
+/**
+ * ****************************************************************************
+ * Purpose: This class contains main method where the program execution is
+ *          carried out.
+ *
+ * @author Syeda Naziya
+ * @version 1.0
+ * @since 17-06-2021
+ * *****************************************************************************
+ */
+
 package regex;
 
 public class UserRegistrationMain {
 
+    static UserRegistration user = new UserRegistration();
+
+
     public static void main(String[] args) {
-        System.out.println(" Welcome to User Registration problem ");
+        user.validateFirstName();
     }
 }
